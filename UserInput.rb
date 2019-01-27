bb08e56eece5dbad4bfc56447540584d040a3702
Daniel Budziak <danielbudziak@lewisu.edu>
2019-01-27 18:46:18 +0000

    First Assignment Done

---
+++ UserInput.rb
@@ -0,0 +1,12 @@
+print "Enter the first number "
+a = gets.chomp
+
+print "Enter the second number "
+b = gets.chomp
+
+a = a.to_i
+b = b.to_i
+
+print "\nThe numbers added are: #{a+b}"
+print "\nThe numbers subtracted are: #{a-b}"
+print "\nThe numbers multiplied are: #{a*b}"
