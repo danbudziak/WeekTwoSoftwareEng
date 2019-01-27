c368b2856ed236ec161057f157e5269dad2563cc
Daniel Budziak <danielbudziak@lewisu.edu>
2019-01-27 20:58:14 +0000

    Third assignment finshed

---
+++ Santa.rb
@@ -0,0 +1,31 @@
+naughty_list = Hash.new
+
+naughty_list = {
+    "Danny" => "nice",
+    "Jessica" => "naughty",
+    "Linda" => 'naughty'
+}
+
+naughty_list.each do |key, value|
+    print "\n#{key} is #{value}."
+end
+
+print "\nWould you like to enter a new person Santa?"
+print "\nY-Yes N-No "
+add = gets.chomp
+
+if add == 'Y' || add == 'y'
+    print "\nEnter the persons name: "
+    name = gets.chomp
+    print "Enter if they were naughty or nice: "
+    value = gets.chomp
+    naughty_list[name] = value
+    
+    print "\n\nThe new list is: "
+    naughty_list.each do |key, value|
+        print "\n#{key} is #{value}."
+    end
+    
+elsif add = 'N' || 'n'
+    print "\n\nThanks for checking in Santa!"
+end
